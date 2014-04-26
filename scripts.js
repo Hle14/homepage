d3d81893664a20629d32f6b383ddb6b538efcab6
Hle14 <HansLe518@gmail.com>
2014-04-26 14:36:39 -0400

    separating stylesheets and scripts
    scripts.js and stylesheets.css contain code formerly in index.html

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*** Temporary text filler function. Remove when deploying template. ***/
+var gibberish=["This is just some filler text", "Welcome to Dynamic Drive CSS Library", "Demo content nothing to read here"]
+function filltext(words){
+for (var i=0; i<words; i++)
+document.write(gibberish[Math.floor(Math.random()*3)]+" ")
+}
+
+var d = new Date();
+
+function fadeIn(element_id){
+	//quadratic function controls alpha level
+	var element = document.getElementById(element_id);
+	var time0 = d.getTime();
+	var x = 0;
+	var alpha = 0;
+	while(x < 1.0) //changes alpha level over time
+	{
+		x = (d.getTime() - time0) / 1000;
+		alpha = -100*x*x + 200*x;
+		element
+	}
+}
