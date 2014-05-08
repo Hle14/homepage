b5db5ceb4c615df491eedf5aaabe41aedcad1a1e
Hle14 <HansLe518@gmail.com>
2014-05-08 11:30:50 -0400

    adding initial menu animation
    page loads all gray, click to show menu

--- scripts.js
+++ scripts.js
@@ -55,12 +55,58 @@ var changeShyGuy = function(){
 //elements to load - at the end of the last fade in fadeEffect.tween will call itself using the next item in the array
 var loads = new Array("portrait","menu_name","btn-home","btn-projects","btn-blog","btn-links","btn-contact");
 
+var menu_bar;
+var showMenu = function(){
+	//alert("CALLED");
+	var width = window.innerWidth;
+	var steps = Math.sqrt(width - 200);
+	//alert(steps);
+	var delay = 1;
+	//menu_bar.style['-webkit-animation'] = "readyMenu 0.66s";
+    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome')
+	if(is_chrome > 1) {
+		alert("aHA!");
+		// is Google chrome 
+		menu_bar.style['-webkit-animation'] = "readyMenu 0.66s";
+	}else{ 
+		alert("haAH!");
+		// not Google chrome 
+		menu_bar.style['animation'] = "readyMenu 0.66s";
+	}
+	menu_bar.style.width = "200px";
+/*	while(delay<steps)
+	{
+		width -= delay;
+		setTimeOut(function()
+			{
+				menu_bar.style.width = width;
+			},delay);
+		delay++;
+	}
+//	menu_bar.style.width = width + "%";
+/*
+	alert(value);
+	var delay = 1;
+	//temporary function to animate menu
+	var animateMenu = function(){
+		//menu_bar
+	};
+	while(delay<41)
+	{
+		setTimeOut(function(){animateMenu()},delay);
+		delay++;
+	}
+	*/
+};
 
 window.onload = function(){
-
+	menu_bar = document.getElementById("framecontent");
+	menu_bar.addEventListener("click",showMenu,false);
+/*
+	menu_bar.removeEventListener("click",showMenu,false);
 	//could you pass "for each x in loads"?
 	fadeEffect.init(loads[loads_index],3,100);
-
+*/
 	var home = document.getElementById("btn-home");
 	var proj = document.getElementById("btn-projects");
 	var blog = document.getElementById("btn-blog");
